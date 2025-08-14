9ff20ec8c64e61ee68476fcda6c74766b1c3c640
giftdee <discordbotz526@gmail.com>
2025-08-14 14:33:54 +0300

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -3,14 +3,14 @@ const fetch = require('node-fetch');
 async function lyricsCommand(sock, chatId, songTitle) {
     if (!songTitle) {
         await sock.sendMessage(chatId, { 
-            text: '🔍 Please enter the song name to get the lyrics! Usage: *lyrics <song name>*'
+            text: '🥹 huh enter the song name to get the lyrics! Usage: *lyrics <song name>*'
         });
         return;
     }
 
     try {
         // Fetch song lyrics using the some-random-api.com API
-        const apiUrl = `https://api.lyrics.ovh/v1/=${encodeURIComponent(songTitle)}`;
+        const apiUrl = `https://api.giftedtech.web.id/api/search/lyrics?apikey=gifted&query=${encodeURIComponent(songTitle)}`;
         const res = await fetch(apiUrl);
 
         if (!res.ok) {
@@ -19,21 +19,21 @@ async function lyricsCommand(sock, chatId, songTitle) {
 
         const json = await res.json();
 
-        if (!json.lyrics) {
+        if (!json.result.lyrics) {
             await sock.sendMessage(chatId, { 
-                text: `❌ Sorry, I couldn't find any lyrics for "${songTitle}".`
+                text: `😪 huh, I couldn't fetch any lyrics for "${songTitle}".`
             });
             return;
         }
 
         // Sending the formatted result to the user
         await sock.sendMessage(chatId, {
-            text: `🎵 *Song Lyrics* 🎶\n\n▢ *Title:* ${json.title || songTitle}\n▢ *Artist:* ${json.author || 'Unknown'}\n\n📜 *Lyrics:*\n${json.lyrics}\n\nHope you enjoy the music! 🎧 🎶`
+            text: `🎵 *Song Lyrics* 🎶\n\n▢ *Title:* ${json.result.title || songTitle}\n▢ *Artist:* ${json.result.artist || 'Unknown'}\n\n📜 *Lyrics:*\n${json.result.lyrics}\n\nHope you enjoy the music! 🎧 🎶`
         });
     } catch (error) {
         console.error('Error in lyrics command:', error);
         await sock.sendMessage(chatId, { 
-            text: `❌ An error occurred while fetching the lyrics for "${songTitle}".`
+            text: `💠 unexpected error occurred while hunting the lyrics for "${songTitle}".`
         });
     }
 }

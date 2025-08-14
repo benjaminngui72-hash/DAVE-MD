dbf8758ff9e915516c098c6fac57be7c83ba1db9
giftdee <discordbotz526@gmail.com>
2025-08-14 06:36:26 +0300

    Create settings.js

---
+++ setting/settings.js
@@ -0,0 +1,57 @@
+const fs = require('fs')
+
+//~~~~~~~~~~~ Settings Owner ~~~~~~~~~~~//
+global.owner = "254104260236"
+global.developer = "254104260236"
+global.bot = ""
+global.devname = "Dave"
+global.ownername = "Gifted dave"
+global.botname = "𝐃𝐀𝐕𝐄-𝐗𝐌𝐃"
+global.versisc = "2"
+global.packname = "𝐃𝐀𝐕𝐄-𝐗𝐌𝐃"
+//~~~~~~~~~~~ Settings Sosmed ~~~~~~~~~~~//
+global.linkwa = "https://wa.me/254104260236"
+global.linkyt = "https://www.youtube.com/Davke"
+global.linktt = "https://tiktok.com"
+global.linktele = "https://t.me"
+
+//~~~~~~~~~~~ Settings Bot ~~~~~~~~~~~//
+global.prefix = ["","!",".",",","#","/","🗿","💠"]
+global.autoRecording = false
+global.autoTyping = true
+global.autorecordtype = false
+global.autoread = true
+global.autobio = false
+global.anti92 = false
+global.owneroff = false
+global.autoswview = true
+
+//~~~~~~~~~~~ Settings Thumbnail ~~~~~~~~~~~//
+global.thumbbot = "https://files.catbox.moe/lidsgj.jpg"
+global.thumbown = "https://files.catbox.moe/lidsgj.jpg"
+
+//~~~~~~~~~~~ Settings Channel ~~~~~~~~~~~//
+global.idchannel = "120363400480173280@newsletter*"
+global.channelname = "DAVE-XMD UPDATES"
+global.channel = "https://whatsapp.com/channel/0029VbApvFQ2Jl84lhONkc3k"
+
+//~~~~~~~~~~~ Settings Message ~~~~~~~~~~~//
+global.mess = {
+  developer: " `[ Developer Only!! ]` \n This feature is for developers only!!",
+  owner: " `[ Owner Only!! ]` \n This feature is for owners only!!",
+  group: " `[ Group Only!! ]` \n This feature is for group chats only!!",
+  private: " `[ Private Only!! ]` \n This feature is for private chats only!!",
+  admin: " `[ Admin Only!! ]` \n This feature is for admins only!!",
+  botadmin: " `[ Bot Admin Only!! ]` \n This feature is for bot admins only!!",
+  wait: " `[ Wait!! ]` \n Please wait, loading...",
+  error: " `[ Error!! ]` \n An error occurred!!",
+  done: " `[ Done!! ]` \n Process completed!!"
+}
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+})
